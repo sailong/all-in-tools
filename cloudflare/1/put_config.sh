31fa2b9c0ae064604617cb3dd330cbd9c0f85723
changsailong <shailong123@126.com>
2026-01-10 23:08:46 +0800

    eat(cloudflare): 添加Cloudflare隧道配置管理脚本
    新增get_config.sh和put_config.sh两个脚本，用于通过API获取和更新
    Cloudflare隧道配置。包含账户认证信息配置和日志输出功能，
    并提供readme文档说明使用方法。

---
+++ cloudflare/1/put_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ACCOUNT_ID=account id
+TUNNEL_ID=tunnel id
+ACCOUNT_EMAIL=email
+ACCOUNT_KEY=Global API Key
+
+curl https://api.cloudflare.com/client/v4/accounts/$ACCOUNT_ID/cfd_tunnel/$TUNNEL_ID/configurations \
+    -X PUT \
+    -H 'Content-Type: application/json' \
+    -H "X-Auth-Email: $ACCOUNT_EMAIL" \
+    -H "X-Auth-Key: $ACCOUNT_KEY" \
+    -d '{}' > upd.log
